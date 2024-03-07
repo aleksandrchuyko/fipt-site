585ec8d11dd872a0e0f54d1e79c6d17d03d21a5e
aleksandrchuyko <chao.mess@gmail.com>
2024-03-07 21:36:48 +0200

    upd

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'https://btc-test-backend.onrender.com/api/';
+// export const BASE_URL = 'https://btc-test-backend.onrender.com/api/';
+
+export const BASE_URL = 'http://localhost:3001/api/';
